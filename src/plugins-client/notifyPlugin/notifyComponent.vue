a8b131d12b1b9d3d0424c61da2d311abac3f4535
ggyoz <ggyoz@naver.com>
2022-09-23 18:05:00 +0900

    notify component 추가1

---
+++ src/plugins-client/notifyPlugin/notifyComponent.vue
@@ -0,0 +1,50 @@
+<template>
+    <v-dialog v-model="dialog" :max-width="options.width">
+        <v-card>
+            <v-toolbar color="primary" dark>
+                <v-toolbar-title>{{title}}</v-toolbar-title>
+            </v-toolbar>
+            <v-card-text class="pt-4">
+                <v-icon large :color="options.iconColor" class="mr-4">{{options.icon}}</v-icon>                
+                {{ content }}
+                <v-form v-if="options.type === 'prompt'" v-model="valid" lazy-validation>                     
+                    <v-text-field v-model="text" :rules="[ v => !!v || '필수입력입니다.']"></v-text-field>
+                </v-form>
+            </v-card-text>
+            <v-card-actions class="justify-end">
+                <v-btn v-if="options.type !== 'alert'">취소</v-btn>
+                <v-btn @click="confrimBtn" color="primary">확인</v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
+</template>
+
+<script>
+    export default{
+        name:"notifyComponent",        
+        data(){
+            return{
+                dialog:true, 
+                title : "타이틀",
+                content : "내용입니다.",
+                text : "사용자 답변",
+                options: {
+                    width:500,
+                    icon:'mdi-alert',
+                    iconColor:'orange',
+                    type:'confirm'                    
+                }
+            }
+        },
+        methods:{
+            confrimBtn(){
+                this.dialog = false
+            }
+        }
+
+    }
+</script>
+
+<style>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -15,39 +15,48 @@
       <v-btn @click="barTest2">End</v-btn>
       <v-btn @click="barTest3">fail</v-btn>
     </div>
-    
+
+    <h1>Notify Test</h1>
+    <div>
+      <v-btn @click="notifyTest1">Alert</v-btn>
+    </div>
+    <NotifyComponent/>
   </div>
 </template>
 
 <script>
+import NotifyComponent from '@/plugins-client/notifyPlugin/notifyComponent.vue';
   export default {
-    name: 'Home',
-    methods :{
-      toastTest1(){
-        this.$toast.info('hello Info');
-      },
-      toastTest2(){
-        this.$toast.success('hello Info');
-      },
-      toastTest3(){
-        this.$toast.error('hello Info');
-      },
-      toastTest4(){
-        this.$toast.warning('hello Info');
-      },
-      toastTest5(){
-        throw new Error("전역 에러!!")
-      },
-      barTest1(){
-        this.$Progress.start();
-      },
-      barTest2(){
-        this.$Progress.finish();
-      },
-      barTest3(){
-        this.$Progress.fail();
-      }
-
-    }
-  }
+    name: "Home",
+    methods: {
+        toastTest1() {
+            this.$toast.info("hello Info");
+        },
+        toastTest2() {
+            this.$toast.success("hello Info");
+        },
+        toastTest3() {
+            this.$toast.error("hello Info");
+        },
+        toastTest4() {
+            this.$toast.warning("hello Info");
+        },
+        toastTest5() {
+            throw new Error("전역 에러!!");
+        },
+        barTest1() {
+            this.$Progress.start();
+        },
+        barTest2() {
+            this.$Progress.finish();
+        },
+        barTest3() {
+            this.$Progress.fail();
+        },
+        notifyTest1() {
+            alert("message");
+        }
+    },
+    components: { NotifyComponent }
+}
 </script>
